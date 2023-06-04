0c9b9adac582134794967c949b5fc38fdcec2327
Nadezda <nadenka.merkulowa@yandex.ru>
2023-06-04 23:35:54 +0500

    добавили функцию ( цикл=длине массива, в цикле условие если элимент <3сохраняем в счетчик*

--- задача/Program.cs
+++ задача/Program.cs
@@ -6,3 +6,15 @@
 ﻿string[] array1 = new string[5] {"1", "тридцать", "25", "сто", "десять"};
 string[] array2 = new string[array1.Length];
  int count=0;
+ void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
